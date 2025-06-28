0d7f74ca1ce775427272367245e370e628f49999
jasur737 <baxtiyorovjasur728@gmail.com>
2025-06-28 10:22:21 +0500

    ;)

---
+++ app.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: false,
+    slidesPerView: 2,
+    spaceBetween: 25,
+  
+    // If we need pagination
+    
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+   
+  });
